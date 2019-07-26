9ea6139a2edf3a10a6599a988b6c22be1420b51e
Skip Hendriks <skappa@localhost.localdomain>
2019-07-26 16:34:28 +0930

    Main functionality

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-'use strict';
+import express from 'express';
+import cors from 'cors';
+import multer from 'multer';
 
-var express = require('express');
-var cors = require('cors');
+const upload = multer({ dest: 'uploads/' });
 
-// require and use "multer"...
-
-var app = express();
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', (req, res) => res.sendFile(`${process.cwd()}/views/index.html`));
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  const { originalname: name, mimetype: type, size } = req.file;
+  res.json({
+    name,
+    type,
+    size,
   });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Node.js listening ...');
 });
